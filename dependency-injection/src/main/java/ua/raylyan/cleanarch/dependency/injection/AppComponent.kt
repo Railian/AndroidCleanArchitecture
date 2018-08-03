a185de113464ba79eef51afc1b1de297989453d7
EvgeniyRaylyan <evgeniy.raylyan@chariot.com>
2018-08-03 12:22:58 +0300

    merge "master" into "develop"

--- dependency-injection/src/main/java/ua/raylyan/cleanarch/dependency/injection/AppComponent.kt
+++ dependency-injection/src/main/java/ua/raylyan/cleanarch/dependency/injection/AppComponent.kt
@@ -3,7 +3,6 @@ package ua.raylyan.cleanarch.dependency.injection
 import dagger.Component
 import javax.inject.Singleton
 
-
 @Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
